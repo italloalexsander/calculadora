71ffaff7bfc7c49cae755631ed49401fdd1be2e9
italloalexsander <italloalexsander@gmail.com>
2022-03-31 11:19:22 -0300

    inicio e fim

---
+++ calculadora.js
@@ -0,0 +1,104 @@
+//Limpa a tela
+document.querySelector("#clear").addEventListener("click", ()=>{
+    document.querySelector("#display").value ="";
+})
+
+//Coloca 1 na tela
+const um = () =>{
+    if(document.querySelector("#display").value == ""){
+        document.querySelector("#display").value = "1";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "1"
+    }
+}
+
+//Coloca 2 na tela
+const dois = () =>{
+    if(document.querySelector("#display").value == ""){
+        document.querySelector("#display").value = "2";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "2"
+    }
+}
+
+//Coloca 3 na tela
+const três = () =>{
+    if(document.querySelector("#display").value == ""){
+        document.querySelector("#display").value = "3";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "3"
+    }
+}
+
+//Coloca 4 na tela
+const quatro = () =>{
+    if(document.querySelector("#display").value == ""){
+        document.querySelector("#display").value = "4";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "4"
+    }
+}
+
+//Coloca 5 na tela
+const cinco = () =>{
+    if(document.querySelector("#display").value == ""){
+        document.querySelector("#display").value = "5";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "5"
+    }
+}
+
+//Coloca 6 na tela
+const seis = () =>{
+    if(document.querySelector("#display").value == ""){
+        document.querySelector("#display").value = "6";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "6"
+    }
+}
+
+//Coloca 7 na tela
+const sete = () =>{
+    if(document.querySelector("#display").value == ""){
+        document.querySelector("#display").value = "7";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "7"
+    }
+}
+
+//Coloca 8 na tela
+const oito = () =>{
+    if(document.querySelector("#display").value == ""){
+        document.querySelector("#display").value = "8";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "8"
+    }
+}
+
+//Coloca 9 na tela
+const nove = () =>{
+    if(document.querySelector("#display").value == ""){
+        document.querySelector("#display").value = "9";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "9"
+    }
+}
+
+//Coloca 0 na tela
+const zero = () =>{
+    if(document.querySelector("#display").value == ""){
+        document.querySelector("#display").value = "";
+    }
+    else{
+        document.querySelector("#display").value = document.querySelector("#display").value + "0"
+    }
+}
